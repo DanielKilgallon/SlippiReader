fad45df1a02543aebe6168dc7cff7de0acd5da8d
Daniel Kilgallon <danielkilgallon2@gmail.com>
2020-10-14 23:01:49 -0400

    changing to winrate

--- SlippiReader.js
+++ SlippiReader.js
@@ -10,25 +10,40 @@ const files = fs.readdirSync(gamesDir);
 // convert files to slippi game objects
 const games = files.map((e) => new SlippiGame(gamesDir + e));
 
-var output = "";
-
-var counter = 1
-// console.log(JSON.stringify(games[0].getStats().combos.filter(combo => combo.didKill)));
+let counter = 1;
 // console.log(Object.getOwnPropertyNames(games[0].getMetadata()));
-// console.log(games[0].getMetadata().lastFrame);
+var winsCount = 0;
+const playerName = "Slick Revolver";
 games.forEach(game => {
-    console.log("Game: " + counter++);
-    const combos_that_killed = game.getStats().combos.filter(combo => combo.didKill);
-    combos_that_killed.forEach(combo => {
-        if (combo.playerIndex == 1) {
-            console.log("-You:");
-        } else {
-            console.log("-Opponent:");
+    if (JSON.stringify(game.getMetadata().players["0"].names) != JSON.stringify({})) {
+        counter++;
+        var playerOneName = game.getMetadata().players["0"].names.netplay
+        var playerTwoName = game.getMetadata().players["1"].names.netplay
+        if (playerName == playerOneName && game.getStats().overall[0].killCount > game.getStats().overall[1].killCount) {
+            winsCount++;
+        } else if (playerName == playerTwoName && game.getStats().overall[0].killCount < game.getStats().overall[1].killCount) {
+            winsCount++;
         }
-        combo.moves.forEach(move => {
-            console.log("--" + moves.getMoveName(move.moveId));
-        });
+        console.log("wins: " + winsCount);
+        console.log("games:" + counter);
+        console.log(((winsCount / counter) * 100) + "%");
         console.log();
-    });
-    console.log();
+    }
 });
+
+//     console.log("Game: " + counter++);
+//     const combos_that_killed = game.getStats().combos.filter(combo => combo.didKill);
+//    if (combos_that_killed) {
+//         combos_that_killed.every(combo => {
+//             if (combo.playerIndex == 1) {
+//                 console.log("-You:");
+//             } else {
+//                 console.log("-Opponent:");
+//             }
+//             combo.moves.forEach(move => {
+//                 console.log("--" + moves.getMoveName(move.moveId));
+//             });
+//             console.log();
+//         });
+//     }
+//     console.log();
